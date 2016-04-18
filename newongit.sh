87b72a847dcee0dda33b2aa2c33b2a639a9cddda
josy1024 <josy1024@gmail.com>
2016-04-18 21:13:29 +0200

    doku for git

---
+++ newongit.sh
@@ -0,0 +1,8 @@
+git commit -m 'init import'
+git config --global user.email "josy1024@gmail.com"
+git config --global user.name "josy1024"
+git commit -m 'init import'
+git remote add origin https://github.com/josy1024/environmentPI.git
+git pull origin master
+git push origin master
+
