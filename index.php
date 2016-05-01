b971da8cc21adcfb014298556d6ea8a17e030356
Lahmer Josef (gugler* gmbh) <josef.lahmer@gugler.at>
2016-05-01 20:04:02 +0200

    title

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $w=800;
 <header>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="basicresponsive.css">
+ <title>smart home</title>
 </header>
 <body>
 <h1>smart home</h1>
