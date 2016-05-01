10a8b36892ca6c814fdd4096d7888209d73b96c8
Lahmer Josef (gugler* gmbh) <josef.lahmer@gugler.at>
2016-05-01 13:49:35 +0200

    schalter abfrage

--- 443/on.php
+++ 443/on.php
@@ -3,11 +3,14 @@
 
 #if (isset($_GET["on"])) {$on=$_GET["on"];}
 
-$schalter = 2;
+
 $on = 1;
+$schalter = 2;
 $installid="11111";
 
-if (isset($_GET["on"])) {$on=$_GET["on"];}
+if (isset($_GET["on"])) {$on = intval($_GET["on"]);}
+
+if (isset($_GET["s"])) {$schalter= intval($_GET["s"]);}
 
 $prg="sudo /opt/433Utils/RPi_utils/send $installid $schalter $on";
 
