c2495a4d6f4693997ab69f649401bc8c51806a7e
josy1024 <josy1024@gmail.com>
2016-05-20 18:48:50 +0200

    lüften!

--- airsensor.sh
+++ airsensor.sh
@@ -5,3 +5,10 @@
 TEMPDIR=/opt/temp
 
 /opt/airsensor/./airsensor -o -v >  $TEMPDIR/airsensor.txt
+
+VALUE=`cat $TEMPDIR/airsensor.txt`
+
+if [ $(echo "if (${VALUE} > 500) 1 else 0" | bc) -eq 1 ]; then
+    /usr/bin/php /opt/temp/bot/tele.php "bitte lueften luft schlecht ${VALUE}"
+fi
+
