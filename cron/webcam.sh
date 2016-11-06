3d597ad929f21aee90775be0df6056c839de093c
josy1024 <josy1024@gmail.com>
2016-11-06 12:36:33 +0100

    WEBCAM WITH CURL SEND! ;-)

--- cron/webcam.sh
+++ cron/webcam.sh
@@ -9,11 +9,14 @@ BOT=/opt/temp/bot
 
 /usr/bin/fswebcam -r"1920x1080" $OUTPUT/latest.jpg 2> $OUTPUT/latest.txt
 
+. /opt/secure/telegram.sh
 
-/usr/bin/php $BOT/attach.php $OUTPUT/latest.jpg
+/usr/bin/curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="$ROOM_ID" -F photo="@$OUTPUT/latest.jpg"
+
 
 #SnapFile=$OUTPUT/latest.jpg
 #curl -s -X POST "https://api.telegram.org/bot--BOTID--/sendPhoto" -F chat_id="--CHATID--" -F photo="@$SnapFile"
+#/usr/bin/php $BOT/attach.php $OUTPUT/latest.jpg
 
 # problem in escape string! ;
 #LOGTEXT=`cat $OUTPUT/latest.txt`
