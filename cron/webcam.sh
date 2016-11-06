6a4eeb345fef43cd46a7f20e6a869db561f557fa
josy1024 <josy1024@gmail.com>
2016-11-06 12:41:29 +0100

    COMMENTS

--- cron/webcam.sh
+++ cron/webcam.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-# webcam foto
 
+### WEBCAM FOTO MACHEN
 OUTPUT=/var/www/html/cam
-BOT=/opt/temp/bot
-
 # -v verbose?
 # -r resolution
 
 /usr/bin/fswebcam -r"1920x1080" $OUTPUT/latest.jpg 2> $OUTPUT/latest.txt
 
-. /opt/secure/telegram.sh
 
+# CONFIG variablen für TELEGRAM BOT API:
+. /opt/secure/telegram.sh
+# enhaelt
+#BOT_TOKEN=
+#ROOM_ID=
+
+### FOTO UPLOAD TO TELEGRAM API
 /usr/bin/curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="$ROOM_ID" -F photo="@$OUTPUT/latest.jpg"
 
 
+###### ENTWICKLUNGSCODE....
 #SnapFile=$OUTPUT/latest.jpg
 #curl -s -X POST "https://api.telegram.org/bot--BOTID--/sendPhoto" -F chat_id="--CHATID--" -F photo="@$SnapFile"
 #/usr/bin/php $BOT/attach.php $OUTPUT/latest.jpg
