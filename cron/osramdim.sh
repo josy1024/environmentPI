51b09862b388f2356a8c4d3eea154112d909cf37
josef lahmer <josy1024@gmail.com>
2019-04-05 19:09:59 +0200

    Create osramdim.sh

---
+++ cron/osramdim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/bin/python3 /opt/osram/lightify_scripts/osram_control.py  -l 'wohnzimmerlampe' -a dim
+/usr/bin/python3 /opt/osram/lightify_scripts/osram_control.py  -l 'kuechespeis' -a dim
+/usr/bin/python3 /opt/osram/lightify_scripts/osram_control.py  -l 'kuecheregal' -a dim
+/usr/bin/python3 /opt/osram/lightify_scripts/osram_control.py  -l 'esstischlampe' -a dim
